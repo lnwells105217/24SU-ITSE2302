ee3f55db5c87e9f32c16e1935a363bab09f5f598
lnwells105217 <lnwells105217@mymail.tstc.edu>
2024-05-15 14:06:23 -0500

    Uploaded files for M1 Mastery
    Files uploaded include the websites wireframe design, the HTML, CSS, and JavaScript documents for the site, and images for the website.

---
+++ M1Mastery/JavaScript/main.js
@@ -0,0 +1,12 @@
+/* Variable initialization */
+var firstName = "Lynsey";
+var lastName = "Wells";
+var sentence = "Praying mantis always eat their prey alive, starting from the head.";
+var theTruth = true;
+var numTen = 10, numFifteen = 15, numTwentyFive = 25; 
+
+/* Print to browser console */
+console.log(firstName + " " + lastName);
+console.log("The length of my sentence is: " + sentence.length + " characters long.");
+console.log("The sum of " + numTen + " and " + numFifteen + " is equal to " + numTwentyFive + ".");
+console.log("The variable theTruth is storing the value " + theTruth + ". The data type of theTruth is " + typeof(theTruth) + ".");ß
